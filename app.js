75ccb75a8810a3860ecf3eada468be16fb800a55
Lenin Fernandez <fernandezplenin@gmail.com>
2020-10-23 14:05:56 -0300

    moved db connection function to app.js

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 const express = require('express');
 const app = express();
 const characters = require('./routes/characters');
+const mongoose = require('mongoose');
 
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION 🔥 Shooting down...');
+    process.exit(1);
+});
+
+mongoose.connect('mongodb://tagtechUser:PassWorD@localhost:27017/tagtech', 
+    { useNewUrlParser: true , useUnifiedTopology: true }, () => {
+        console.log("Connected to DB");
+    });
 
 app.use('/characters', characters);
+
 
 // Routes
 app.get('/', (req, res) => {
@@ -12,3 +24,11 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000);
+
+process.on('unhandledRejection', err => {
+    console.log(err);
+    console.log('UNHANDLED REJECTION 🔥 Shooting down.');
+    server.close(() => {
+      process.exit(1);
+    });
+  });
--- routes/characters.js
+++ routes/characters.js
@@ -1,5 +1,4 @@
 "use strict";
-const mongoose = require('mongoose');
 const express = require('express');
 const Character = require('../models/character');
 const https = require('https');
@@ -22,26 +21,19 @@ router.route('/sync')
         });
         remoteRes.on('end', function () {
             if (remoteRes.statusCode === 200) {
-                try {
-                    mongoose.connect('mongodb://tagtechUser:PassWorD@localhost:27017/tagtech', { useNewUrlParser: true , useUnifiedTopology: true }, () => 
-                    {
-                        console.log('Connected to DB')
-                        var data = JSON.parse(json);
-                        
-                        Character.insertMany(data).then(function(){
-                            return res.send("Characters updated");
-                        }).catch(function(error){
-                            return res.send("Error updating characters: ", error);
-                            console.log(error);
-                        }); 
-                });
-                } catch (e) {
-                    console.log('Error connecting to DB');
-                }
-            } else {
+                var data = JSON.parse(json);
+                
+                Character.insertMany(data).then(function(){
+                    return res.send("Characters updated");
+                }).catch(function(error){
+                    return res.send("Error updating characters: ", error);
+                    console.log(error);
+                }); 
+            }
+            else {
                 console.log('Error downloading characters from API: ', remoteRes.statusCode);
             }
-        });
+            });
     }).on('error', function (err) {
             console.log('Error:', err);
         });
@@ -49,46 +41,31 @@ router.route('/sync')
 
 router.route('/')
 .get((req, res) => {
-    try {
-        mongoose.connect('mongodb://tagtechUser:PassWorD@localhost:27017/tagtech', { useNewUrlParser: true , useUnifiedTopology: true }, () => 
-        {
-            Character.find({}, function(err, characters) {
-                var characterMap = {};
-            
-                characters.forEach(function(character) {
-                  characterMap[character._id] = character;
-                });
-            
-                res.send(characterMap);  
-              });
+    Character.find({}, function(err, characters) {
+        var characterMap = {};
+        characters.forEach(function(character) {
+            characterMap[character._id] = character;
+        });
+        res.send(characterMap);  
     });
-    } catch (e) {
-        console.log('Error connecting to DB');
-    }
 });
 router.route('/:id')
 .get((req, res) => {
     var id = req.params.id;
     try {
-        mongoose.connect('mongodb://tagtechUser:PassWorD@localhost:27017/tagtech', { useNewUrlParser: true , useUnifiedTopology: true }, () => 
-        {
-            try {
-                Character.findById(id, function(err, character) {
-                    if (character != null) {
-                        return res.send(character);
-                    }else{
-                        return res.status(403).send("There's no character with that id");
-                    }
-                    
-                });
+        Character.findById(id, function(err, character) {
+            if (character != null) {
+                return res.send(character);
             }
-            catch(err){
-                  console.log("Error retrieving character with id: ", id, ". " , err);
-                  return res.status(500).send(err);
-            }
-    });
-    } catch (e) {
-        console.log('Error connecting to DB');
+            else
+            {
+                return res.status(403).send("There's no character with that id");
+            }           
+        });
+    }
+    catch(err){
+        console.log("Error retrieving character with id: ", id, ". " , err);
+        return res.status(500).send(err);
     }
 });
 
